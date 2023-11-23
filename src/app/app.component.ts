51e16c2aec69976a4c54652201ea975a6bf982f8
Thirawat sutalungka <thirawat@crmc.consulting>
2023-11-24 04:06:12 +0700

    Update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { EditorState } from '@codemirror/state';
+import { EditorView, keymap, gutter, lineNumbers, highlightActiveLineGutter, highlightActiveLine, ViewUpdate } from "@codemirror/view"
+import {defaultHighlightStyle, syntaxHighlighting} from "@codemirror/language";
+import { defaultKeymap } from "@codemirror/commands"
+import { xml } from "@codemirror/lang-xml"
+import { autocompletion } from '@codemirror/autocomplete';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'prism-transform-ui';
+export class AppComponent implements AfterViewInit{
+  
+  @ViewChild('xmlEditor') xmlEditorElement: any;
+  @ViewChild('prismEditor') prismEditorElement: any;
+  xml: string = '';
+  ngAfterViewInit(): void {
+    this.registerXMLEditor();
+  }
+
+  registerXMLEditor() {
+    let startState = EditorState.create({
+      doc: this.xml,
+      extensions: [keymap.of(defaultKeymap), xml(), lineNumbers(), highlightActiveLineGutter(), highlightActiveLine(), gutter({ class: "cm-mygutter" }), EditorView.updateListener.of((update: ViewUpdate) => {
+        if (update.docChanged) {
+          this.xml = update.state.doc.toString();
+        }
+      }), EditorView.lineWrapping,syntaxHighlighting(defaultHighlightStyle)
+      ],
+    })
+
+    console.log(this.xmlEditorElement.nativeElement);
+    new EditorView({
+      state: startState,
+      parent: this.xmlEditorElement.nativeElement
+    })
+  }
 }
